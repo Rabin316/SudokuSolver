4113a9cc2f7fb7ec50f68ff6e45045a3204c2cf0
Kaneki316 <rabindangol12345@gmail.com>
2023-04-24 20:57:17 +0545

    Added border around 3x3 grid.

--- src/SudokuFrame.java
+++ src/SudokuFrame.java
@@ -1,17 +1,19 @@
-
+import SudokuSolve.SudokuSolve;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SudokuFrame extends JFrame {
+    private JTextField[][] Board;
+
     SudokuFrame() {
         setTitle("SudokuSolver");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
 
         JPanel BoardPanel = new JPanel(new GridLayout(9, 9, 2, 2));
-        JTextField[][] Board = new JTextField[9][9];
+        Board = new JTextField[9][9];
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -20,6 +22,18 @@ public class SudokuFrame extends JFrame {
                 textField.setHorizontalAlignment(JTextField.CENTER);
                 Board[i][j] = textField;
                 BoardPanel.add(textField);
+
+                // Add borders around each 3x3 grid
+                if (i % 3 == 2 && j % 3 == 2) {
+                    textField.setBorder(BorderFactory.createMatteBorder(1, 1, 2, 2, Color.BLACK));
+                } else if (i % 3 == 2) {
+                    textField.setBorder(BorderFactory.createMatteBorder(1, 1, 2, 0, Color.BLACK));
+                } else if (j % 3 == 2) {
+                    textField.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 2, Color.BLACK));
+                } else {
+                    textField.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 0, Color.BLACK));
+                }
+              
             }
         }
         // Buttons Solve and Reset
@@ -28,13 +42,13 @@ public class SudokuFrame extends JFrame {
         JButton solveButton = new JButton("Solve");
         solveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // SolveSudoku();
+                solveSudoku();
             }
         });
         ButtonPanel.add(solveButton);
         resetButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // ResetSudoku();
+                // resetSudoku();
             }
         });
         ButtonPanel.add(resetButton);
@@ -42,6 +56,33 @@ public class SudokuFrame extends JFrame {
         getContentPane().add(ButtonPanel, BorderLayout.SOUTH);
         pack();
         setVisible(true);
+    }
+
+    private void solveSudoku() {
+        int[][] puzzle = new int[9][9];
+
+        // Get the values entered by the user and fill the puzzle array
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                try {
+                    puzzle[i][j] = Integer.parseInt(Board[i][j].getText());
+                } catch (NumberFormatException e) {
+                    puzzle[i][j] = 0;
+                }
+            }
+        }
+
+        // Solve the Sudoku puzzle
+        if (SudokuSolve.solve(puzzle)) {
+            // Update the UI with the solution
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    Board[i][j].setText(Integer.toString(puzzle[i][j]));
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "The puzzle could not be solved.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     public static void main(String[] args) {
