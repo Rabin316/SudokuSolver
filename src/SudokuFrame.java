cce4cc53e45b6a106c615e8e0c836e498664e128
Kaneki316 <rabindangol12345@gmail.com>
2023-04-24 21:34:52 +0545

    Added Border in 3x3 Grid  and reset action

--- src/SudokuFrame.java
+++ src/SudokuFrame.java
@@ -1,4 +1,4 @@
-import SudokuSolve.SudokuSolve;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -33,7 +33,7 @@ public class SudokuFrame extends JFrame {
                 } else {
                     textField.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 0, Color.BLACK));
                 }
-              
+
             }
         }
         // Buttons Solve and Reset
@@ -42,47 +42,25 @@ public class SudokuFrame extends JFrame {
         JButton solveButton = new JButton("Solve");
         solveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                solveSudoku();
+                // solveSudoku();
             }
         });
         ButtonPanel.add(solveButton);
         resetButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // resetSudoku();
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        Board[i][j].setText("");
+                    }
+                }
             }
         });
+
         ButtonPanel.add(resetButton);
         getContentPane().add(BoardPanel, BorderLayout.CENTER);
         getContentPane().add(ButtonPanel, BorderLayout.SOUTH);
         pack();
         setVisible(true);
-    }
-
-    private void solveSudoku() {
-        int[][] puzzle = new int[9][9];
-
-        // Get the values entered by the user and fill the puzzle array
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                try {
-                    puzzle[i][j] = Integer.parseInt(Board[i][j].getText());
-                } catch (NumberFormatException e) {
-                    puzzle[i][j] = 0;
-                }
-            }
-        }
-
-        // Solve the Sudoku puzzle
-        if (SudokuSolve.solve(puzzle)) {
-            // Update the UI with the solution
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    Board[i][j].setText(Integer.toString(puzzle[i][j]));
-                }
-            }
-        } else {
-            JOptionPane.showMessageDialog(this, "The puzzle could not be solved.", "Error", JOptionPane.ERROR_MESSAGE);
-        }
     }
 
     public static void main(String[] args) {
